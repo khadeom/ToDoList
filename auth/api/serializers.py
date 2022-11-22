818d58a0e429a06c47102b00bcfeb4e9aa86ae7c
khadeom <khadeom10@gmail.com>
2022-11-23 04:19:12 +0530

    home page added

--- auth/api/serializers.py
+++ auth/api/serializers.py
@@ -15,30 +15,3 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
-
-class LoginSerializers(serializers.Serializer):
-    username = serializers.CharField(max_length=255)
-    password = serializers.CharField(
-        label=("password"),
-        style={'input_type': 'password'},
-        trim_whitespace=False,
-        max_length=128,
-        write_only=True
-    )
-
-    def validate(self, data):
-        username = data.get('username')
-        password = data.get('password')
-
-        if username and password:
-            user = authenticate(request=self.context.get('request'),
-                                username=username, password=password)
-            if not user:
-                msg = _('Unable to log in with provided credentials.')
-                raise serializers.ValidationError(msg, code='authorization')
-        else:
-            msg = _('Must include "username" and "password".')
-            raise serializers.ValidationError(msg, code='authorization')
-
-        data['user'] = user
-        return data
--- mainapp/api/views.py
+++ mainapp/api/views.py
@@ -6,33 +6,33 @@ from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 # import re
+from rest_framework_extensions.mixins import NestedViewSetMixin
 
+# list all tasks
 class ListToDo(generics.ListAPIView):
     queryset = ToDoList.objects.all()
     authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
-    serializer_class = ToDoListSerializers
-   
+    serializer_class = TaskSerializer
+
+# PUT
 class Detailview(generics.RetrieveUpdateAPIView):
     queryset = ToDoList.objects.all()
     authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
-    serializer_class = ToDoListSerializers
+    serializer_class = TaskSerializer
 
-
+# Create Task
 class Createview(generics.CreateAPIView):
     queryset = ToDoList.objects.all()
     authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
-    serializer_class = ToDoListSerializers
+    serializer_class = TaskSerializer
      
 
-    # def get(self, request, *args, **kwargs):
-    #     return Response("he ha")
-
-
+# Delete task
 class Deleteview(generics.DestroyAPIView):
     queryset = ToDoList.objects.all()
     authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
-    serializer_class = ToDoListSerializers
+    serializer_class = TaskSerializer
