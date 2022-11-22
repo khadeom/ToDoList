d4ea4cd0e02f033b454d9940c435e7086f4d3924
khadeom <khadeom10@gmail.com>
2022-11-23 00:20:52 +0530

    auth added

--- mainapp/api/views.py
+++ mainapp/api/views.py
@@ -3,20 +3,23 @@ from rest_framework import generics, permissions
 from .serializers import *
 from mainapp.models import ToDoList
 from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 # import re
 
 class ListToDo(generics.ListAPIView):
     queryset = ToDoList.objects.all()
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
     serializer_class = ToDoListSerializers
-    
-
+   
 class Detailview(generics.RetrieveUpdateAPIView):
     queryset = ToDoList.objects.all()
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
     serializer_class = ToDoListSerializers
 
 
 class Createview(generics.CreateAPIView):
     queryset = ToDoList.objects.all()
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
     serializer_class = ToDoListSerializers
      
 
@@ -26,4 +29,5 @@ class Createview(generics.CreateAPIView):
 
 class Deleteview(generics.DestroyAPIView):
     queryset = ToDoList.objects.all()
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
     serializer_class = ToDoListSerializers
