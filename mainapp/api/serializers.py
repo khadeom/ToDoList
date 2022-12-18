44cbf4c685eed23bef3cdd8b44174481c78dfd40
khadeom <khadeom10@gmail.com>
2022-12-19 00:07:53 +0530

    a

--- mainapp/api/serializers.py
+++ mainapp/api/serializers.py
@@ -8,8 +8,12 @@ class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = "__all__"
+
 class TaskSerializer(serializers.ModelSerializer):
-    tags = TagSerializer(many=True)
+    tags = serializers.SlugRelatedField(many=True, slug_field='name', read_only=True)
+    update_tags = serializers.ListField(
+        child=serializers.CharField(max_length=30), write_only=True)
+
     class Meta:
         lookup_field = 'id'
         model = ToDoList
@@ -17,23 +21,66 @@ class TaskSerializer(serializers.ModelSerializer):
                     "tags", "status","timestamp")
         read_only_fields = ('id', 'timestamp')
 
+    # def create(self, validated_data):
+    #     tag_names = validated_data.pop('update_tags')
+    #     instance = super().create(validated_data)
+    #     user = self.context['request'].user
+    #     tags = []
+    #     for name in tag_names:
+    #         tag, created = Tag.objects.get_or_create(name=name, defaults={'created_by': user})
+    #         tags.append(tag)
+    #     instance.tags.set(tags)
+    #     return instance
 
-    def create(self, validated_data):
-        tags = validated_data.pop('tags')
-        print(tags)
-        task = ToDoList.objects.create(**validated_data)
+    def update(self, instance, validated_data):
+        print("isme ja bhi rraha ky")
+        tag_names = validated_data.pop('update_tags')
+        instance = super().update(instance, validated_data)
+        user = self.context['request'].user
+        tags = []
+        for name in tag_names:
+            # tag, created = Tag.objects.get_or_create(name=name, defaults={'created_by': user})
+            tag, created = Tag.objects.get_or_create(name=name)
 
-        for tag in tags:
-            print(tag)
-            if Tag.objects.filter(name = tag):
-                pass
-            else:
-                Tag.objects.create(todolist=task, **tag)
+            tags.append(tag)
+        instance.tags.set(tags)
+        return instance
+# class TaskSerializer2(serializers.ModelSerializer):
+#     tags = TagSerializer(many=True)
+#     class Meta:
+#         lookup_field = 'id'
+#         model = ToDoList
+#         fields =("id", "title", "description", "due_date",
+#                     "tags", "status","timestamp")
+#         read_only_fields = ('id', 'timestamp')
 
-        return task
 
-    def validate_due_date(self, value):
-        if (timezone.now().date() > value):
-            raise serializers.ValidationError('deadline must be in the future.')
-        return value
+#     def create(self, validated_data):
+#         tags = validated_data.pop('tags')
+#         print(tags)
+#         task = ToDoList.objects.create(**validated_data)
+
+#         for tag in tags:
+#             print(tag)
+#             if Tag.objects.filter(name = tag):
+#                 pass
+#             else:
+#                 Tag.objects.create(todolist=task, **tag)
+
+#         return task
+#     def update(self, instance, validated_data):
+#         tag = instance.pop('tags')
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.description=validated_data.get('description', instance.description)
+
+#         instance.due_date = validated_data.get('due_date',instance.due_date)
+#         instance.tags = validated_data.get('tags',instance.tags)
+#         instance.status = validated_data.get('status',instance.status)
+#         instance.save()
+#         return instance
+
+#     def validate_due_date(self, value):
+#         if (timezone.now().date() > value):
+#             raise serializers.ValidationError('deadline must be in the future.')
+#         return value
 
--- todo/settings.py
+++ todo/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # apps
     "mainapp",
     "users",
+    "watch"
 ]
 
 MIDDLEWARE = [
